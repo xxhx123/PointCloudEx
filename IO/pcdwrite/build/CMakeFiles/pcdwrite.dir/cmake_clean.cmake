e7fd8f3ce3e5c8b79417b6e393b1755947ab21ac
xxhx123 <1414624463@qq.com>
2024-03-29 11:41:09 +0800

    init

---
+++ IO/pcdwrite/build/CMakeFiles/pcdwrite.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcdwrite.dir/pcdwrite.cpp.o"
+  "pcdwrite.pdb"
+  "pcdwrite"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcdwrite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
