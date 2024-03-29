c4544c58a8dd3f4ae1851af827e7cff2ebbcf651
xxhx123 <1414624463@qq.com>
2024-03-29 10:20:34 +0800

    init

---
+++ pcdread/build/CMakeFiles/pcdread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcdread.dir/pcdread.cpp.o"
+  "pcdread.pdb"
+  "pcdread"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcdread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pcdread/pcdread.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+
+int main ()
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> ("/home/dell/PointCloudEx/pcdread/drill_1.6mm_0_cyb.pcd", *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file drill_1.6mm_0_cyb.pcd \n");
+    return (-1);
+  }
+  std::cout << "Loaded "
+            << cloud->width * cloud->height
+            << " data points from drill_1.6mm_0_cyb.pcd with the following fields: "
+            << std::endl;
+  for (const auto& point: *cloud)
+    std::cout << "    " << point.x
+              << " "    << point.y
+              << " "    << point.z << std::endl;
+  return (0);
+}
+
+
+/* // 下面的代码可以把ply转pcd? 但大小明显不对劲(经测试，有一堆nan数据)
+#include <iostream>
+using namespace std;
+
+#include <pcl/point_types.h> //打开关闭pcd类定义头文件
+#include <pcl/io/pcd_io.h> //所有点类型定义头文件
+#include <pcl/io/ply_io.h> //打开关闭ply类定义头文件
+
+int main()
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PLYReader reader;
+    reader.read<pcl::PointXYZ>("/home/dell/PointCloudEx/ply2pcd/drill_1.6mm_0_cyb.ply", *cloud);
+    pcl::io::savePCDFile("/home/dell/PointCloudEx/ply2pcd/test.pcd",*cloud);
+
+    // std::cout<<"Hello World!"<<std::endl;
+
+    return 0;
+} */
