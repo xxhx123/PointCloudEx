6b9b2fee5963e4dbddfc85883469a10152a02ed3
xxhx123 <1414624463@qq.com>
2024-04-09 23:00:39 +0800

    The cpp code in this branch is based on pcl1.8

---
+++ PCLEx/Octree/octree_search/octree_search.cpp
@@ -0,0 +1,91 @@
+#include <pcl/point_cloud.h>
+#include <pcl/octree/octree_search.h>
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+	
+	// Generate pointcloud data
+	cloud->width = 1000;
+	cloud->height = 1;
+	cloud->points.resize(cloud->width * cloud->height);
+	
+	for (std::size_t i = 0; i < cloud->size(); ++i)
+	{
+		(*cloud)[i].x = 1024.0f * rand() / (RAND_MAX + 1.0f);
+		(*cloud)[i].y = 1024.0f * rand() / (RAND_MAX + 1.0f);
+		(*cloud)[i].z = 1024.0f * rand() / (RAND_MAX + 1.0f);
+	}
+	
+	float resolution = 128.0f;	
+	pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> octree(resolution);
+	octree.setInputCloud(cloud);
+	octree.addPointsFromInputCloud();
+	
+	pcl::PointXYZ searchPoint;
+	searchPoint.x = 1024.0f * rand() / (RAND_MAX + 1.0f);
+	searchPoint.y = 1024.0f * rand() / (RAND_MAX + 1.0f);
+	searchPoint.z = 1024.0f * rand() / (RAND_MAX + 1.0f);
+	
+	// Neighbors within voxel search
+	std::vector<int> pointIdxVec;
+	if(octree.voxelSearch(searchPoint, pointIdxVec))
+	{
+		std::cout << "Neighbors within voxel search at (" << searchPoint.x 
+    	        << " " << searchPoint.y 
+        	    << " " << searchPoint.z
+            	<< ")" << std::endl;
+		for(std::size_t i = 0; i < pointIdxVec.size(); ++i)
+			std::cout << "    "  << (*cloud)[ pointIdxVec[i] ].x 
+			<< " " << (*cloud)[ pointIdxVec[i] ].y
+			<< " " << (*cloud)[ pointIdxVec[i] ].z << std::endl;
+	}
+	
+	// K nearest neighbor search
+	int K = 10;
+	
+	std::vector<int> pointIdxKNNSearch(K);
+	std::vector<float> pointKNNSquaredDistance(K);
+	
+	std::cout << "K nearest neighbor search at (" << searchPoint.x 
+            << " " << searchPoint.y 
+            << " " << searchPoint.z
+            << ") with K=" << K << std::endl;
+
+	if(octree.nearestKSearch(searchPoint, K, pointIdxKNNSearch, pointKNNSquaredDistance) > 0)
+	{
+		for(std::size_t i = 0; i < pointIdxKNNSearch.size(); ++i)
+			std::cout << "    "  << (*cloud)[ pointIdxKNNSearch[i] ].x 
+			<< " " << (*cloud)[ pointIdxKNNSearch[i] ].y
+			<< " " << (*cloud)[ pointIdxKNNSearch[i] ].z
+			<< " (squared distance: " << pointKNNSquaredDistance[i] << ")" << std::endl;
+	}
+	
+	// Neighbors within radius search
+	float radius = 256.0f * rand() / (RAND_MAX + 1.0f);
+	
+	std::vector<int> pointIdxRadiusSearch;
+	std::vector<float> pointRadiusSquaredDistance;
+
+	std::cout << "Neighbors within radius search at (" << searchPoint.x 
+            << " " << searchPoint.y 
+            << " " << searchPoint.z
+            << ") with radius=" << radius << std::endl;
+
+	if(octree.radiusSearch(searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0)
+	{
+		for(std::size_t i = 0; i < pointIdxRadiusSearch.size(); ++i)
+			std::cout << "    "  << (*cloud)[ pointIdxRadiusSearch[i] ].x 
+			<< " " << (*cloud)[ pointIdxRadiusSearch[i] ].y
+			<< " " << (*cloud)[ pointIdxRadiusSearch[i] ].z
+			<< " (squared distance: " << pointRadiusSquaredDistance[i] << ")" << std::endl;
+	}
+	
+	return 0;
+}
